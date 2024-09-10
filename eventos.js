74e688c42d0016ba09d0cd7201d78e4778e9423e
Diana Victoria Delgado <168381355+victoriadelgadobotta@users.noreply.github.com>
2024-09-10 02:13:18 -0300

    solución click

--- eventos.js
+++ eventos.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     let botonDiv = document.getElementById('boton');
 
-    botonDiv.addEventListener('click', function() {
+    botonDiv.addEventListener('click', function(event) {
+
+        if (event.target.tagName === 'BUTTON') {
+            return;
+        }
+        
         alert('Hola! Soy el div');
     });
 
-})
+});
+
+
